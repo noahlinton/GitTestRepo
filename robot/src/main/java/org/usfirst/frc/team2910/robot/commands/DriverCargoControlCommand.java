01442651c13eb4b57bc01c9f7e4261a3a6da729f
Miguel Lopez <Tacotroll@users.noreply.github.com>
2019-07-02 19:39:00 -0400

    Added a new line of text
    The text makes the robot do beep noises

--- robot/src/main/java/org/usfirst/frc/team2910/robot/commands/DriverCargoControlCommand.java
+++ robot/src/main/java/org/usfirst/frc/team2910/robot/commands/DriverCargoControlCommand.java
@@ -4,3 +4,9 @@
 //INSTA CARGO DEPOSIT ACTIVATED
 //Depositing cargo in 3, 2, 1........
 //Cargo has been deposited
+
+
+
+
+
+//Bepp bop beep BOOP BEp bep Bop robot noise Beep boop lettuce beep honk beep
