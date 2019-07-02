9fb5effdf412696148b7d3effaa06420918c1d73
Matt <eglanfield@gmail.com>
2019-07-02 18:33:06 -0400

    removed autonomous folder

--- robot/src/main/java/org/usfirst/frc/team2910/robot/commands/autonomous/StartingOrientation.java
+++
@@ -1,6 +0,0 @@
-package org.usfirst.frc.team2910.robot.commands.autonomous;
-
-public enum StartingOrientation {
-	FORWARDS,
-	SIDEWAYS
-}
--- robot/src/main/java/org/usfirst/frc/team2910/robot/commands/autonomous/stage1/StartingPosition.java
+++
@@ -1,7 +0,0 @@
-package org.usfirst.frc.team2910.robot.commands.autonomous.stage1;
-
-public enum StartingPosition {
-    CENTER,
-    LEFT,
-    RIGHT
-}
