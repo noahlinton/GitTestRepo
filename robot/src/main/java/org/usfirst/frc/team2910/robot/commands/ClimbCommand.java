84e4d4b4287dd1cff8abebe2d395562db45ca28e
Thomas Giovinazzo <thomasgiovinazzo2002@gmail.com>
2019-07-02 20:19:42 -0400

    Update ClimbCommand.java

--- robot/src/main/java/org/usfirst/frc/team2910/robot/commands/ClimbCommand.java
+++ robot/src/main/java/org/usfirst/frc/team2910/robot/commands/ClimbCommand.java
@@ -1,5 +1,2 @@
-//removed file...
-//next year is gonna have a climb
-//Dean loves his climbs too much
-//he should make a game only about climbing
-//thanks rembrants
+//this makes the climb happen
+//yea
