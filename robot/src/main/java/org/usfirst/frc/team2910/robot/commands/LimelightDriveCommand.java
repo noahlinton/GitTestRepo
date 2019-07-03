5fa808202af975a465a6ceec553e08a23783e71c
Matt <eglanfield@gmail.com>
2019-07-02 20:31:45 -0400

    fix conflict

--- robot/src/main/java/org/usfirst/frc/team2910/robot/commands/LimelightDriveCommand.java
+++ robot/src/main/java/org/usfirst/frc/team2910/robot/commands/LimelightDriveCommand.java
@@ -1,7 +1,3 @@
 //How does this flashy green thing work?
-<<<<<<< HEAD
-smoke and mirrors and a little magic.
-=======
 smoke and mirrors and a little magic.
 new branch made
->>>>>>> Darwin's-Branch
