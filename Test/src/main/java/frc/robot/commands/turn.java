a9108e7ae704ef1e5f567292a98bc844dec07583
Zandercat <42007489+Zandercat@users.noreply.github.com>
2019-10-29 16:53:16 -0400

    Added turn command

---
+++ Test/src/main/java/frc/robot/commands/turn.java
@@ -0,0 +1,11 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class turn extends Command {
+    public turn() {
+      // Use requires() here to declare subsystem dependencies
+      requires(Robot.m_subsystem);
+    }
+}
